6b9c3d2f072f6be27ffa78ec1ecec8c5103cc119
Amos-Ditto <amoskip@kabarak.ac.ke>
2022-03-16 03:05:59 +0300

    custom user and db modeling

---
+++ core/models.py
@@ -0,0 +1,78 @@
+from tabnanny import verbose
+from django.db import models
+# from django.utils import timezone
+from django.contrib.auth.models import (
+    BaseUserManager, AbstractBaseUser
+)
+
+# ----------> Custom User model and manager --->
+
+class MyUserManager(BaseUserManager):
+    def create_user(self, email, first_name, last_name,
+            password=None
+        ):
+        if not email:
+            raise ValueError('You must have an email address')
+
+        user = self.model(
+            email = self.normalize_email(email),
+            first_name = first_name,
+            last_name = last_name
+        )
+        user.set_password(password)
+        user.save(using=self._db)
+        return user
+    
+    def create_superuser(self, email, first_name, last_name, password=None):
+        user = self.create_user(
+            email,
+            first_name=first_name,
+            last_name=last_name,
+            password=password
+        )
+        user.is_admin = True
+        user.save(using=self._db)
+        return user
+
+class CustomUser(AbstractBaseUser):
+    email = models.EmailField(verbose_name='email address',
+                max_length=255,
+                unique=True
+            )
+    first_name = models.CharField(max_length=100)
+    last_name = models.CharField(max_length=100)
+    is_active = models.BooleanField(default=True)
+    is_admin = models.BooleanField(default=False)
+
+    objects = MyUserManager()
+
+    USERNAME_FIELD = 'email'
+    REQUIRED_FIELDS = ['first_name', 'last_name']
+
+    def __str__(self):
+        return self.email
+    
+    def has_perm(self, perm, obj=None):
+        return True
+    
+    def has_module_perms(self, app_label):
+        return True
+    
+    @property
+    def is_staff(self):
+        return self.is_admin
+
+# --------------------- End of Custom user and its manager ------->
+
+class Seller_Details(models.Model):
+    user = models.OneToOneField(CustomUser, on_delete=models.CASCADE)
+    county = models.CharField(max_length=100, null=True)
+    town = models.CharField(max_length=150, null=True)
+    phone_number = models.IntegerField(null=True)
+    
+    class Meta:
+        verbose_name = 'Seller Details'
+        verbose_name_plural = 'Seller Details'
+
+    def __str__(self):
+        return '{} | {}'.format(str(self.user.email), str(self.phone_number))
---
+++ core/serializers.py
@@ -0,0 +1,3 @@
+from pyexpat import model
+from attr import fields
+from rest_framework import serializers
