b6f9834bcbad2741e32eefb5264ceb5142cd15b1
Amos Kipyegon <amoskip@kabarak.ac.ke>
2022-12-02 06:10:37 +0300

    feat:  :sparkles:  Setup Client Interface

---
+++ client/nuxt.config.ts
@@ -0,0 +1,24 @@
+import presetIcons from '@unocss/preset-icons'
+
+export default defineNuxtConfig({
+    css: ['~/assets/css/tailwind.css'],
+    modules: ['@nuxtjs/tailwindcss', '@unocss/nuxt', '@nuxtjs/color-mode'],
+    colorMode: {
+        classSuffix: '',
+    },
+    app: {
+        head: {
+            charset: 'utf-16',
+            title: 'Market',
+            meta: [
+                { name: 'description', content: 'Buy or Sell products' },
+                { name: 'viewport', content: 'width=device-width, initial-scale=1.0' },
+            ],
+        },
+        pageTransition: { name: 'page', mode: 'out-in' },
+    },
+    unocss: {
+        icons: true,
+        presets: [presetIcons({})],
+    },
+})
---
+++ client/tailwind.config.js
@@ -0,0 +1,24 @@
+/** @type {import('tailwindcss').Config} */
+
+const colors = require('tailwindcss/colors')
+
+module.exports = {
+    content: ['./components/**/*.{js,vue,ts}', './layouts/**/*.vue', './pages/**/*.vue', './plugins/**/*.{js,ts}', './nuxt.config.{js,ts}'],
+    theme: {
+        extend: {},
+        screens: {
+            xs: '400px',
+            sm: '640px',
+            md: '768px',
+            lg: '1024px',
+            xl: '1280px',
+            '2xl': '1536px',
+        },
+        fontFamily: {
+            sans: ['Nunito', 'sans-serif'],
+            hand: ['Permanent Marker', 'cursive'],
+        },
+    },
+    darkMode: 'class',
+    plugins: [require('@tailwindcss/line-clamp')],
+}
