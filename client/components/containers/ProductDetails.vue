a9515b2cc8e2ccd994068ae259edc1b5d4a5c578
Amos Kipyegon <amoskip@kabarak.ac.ke>
2023-04-19 12:50:14 +0300

    chore: Update Product view page

--- client/components/containers/ProductDetails.vue
+++ client/components/containers/ProductDetails.vue
@@ -1,153 +1,26 @@
 <template>
-	<div class="w-full flex flex-col gap-y-8">
-		<div class="description w-full flex flex-col gap-y-2 ring-1 ring-neutral-200">
-			<button class="w-full bg-custom text-gray-100 flex flex-row justify-between px-8 py-4 text-xl">
-				<div class="flex flex-row gap-x-4 items-center">
-					<div class="w-8 h-8">
-						<svg fill="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg" aria-hidden="true">
-							<path
-								d="M5.625 3.75a2.625 2.625 0 100 5.25h12.75a2.625 2.625 0 000-5.25H5.625zM3.75 11.25a.75.75 0 000 1.5h16.5a.75.75 0 000-1.5H3.75zM3 15.75a.75.75 0 01.75-.75h16.5a.75.75 0 010 1.5H3.75a.75.75 0 01-.75-.75zM3.75 18.75a.75.75 0 000 1.5h16.5a.75.75 0 000-1.5H3.75z"
-							></path>
-						</svg>
-					</div>
-					<span class="text-lg font-medium">Description</span>
-				</div>
+	<div class="w-full flex flex-col">
+		<div
+			class="w-full pt-3 px-2 sm:px-4 rounded-t border border-neutral-200 flex flex-row items-center gap-x-4 sm:gap-x-8 dark:text-neutral-100 dark:border-neutral-600"
+		>
+			<button
+				class="uppercase text-sm sm:text-base font-semibold py-2 px-1 border-b-[3px] border-neutral-800 hover:font-semibold transition-all duration-200 dark:border-neutral-200"
+			>
+				Description
 			</button>
-			<div class="w-full flex flex-col px-4 py-3">
-				<p>
-					Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard
-					dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen
-					book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially
-					unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more
-					recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.
-				</p>
-			</div>
+			<button class="uppercase text-sm sm:text-base font-normal py-2 px-1 hover:font-semibold transition-all duration-200">
+				Customer Reviews (7)
+			</button>
 		</div>
-		<div class="ratings w-full flex flex-col gap-y-2 ring-1 ring-neutral-200">
-			<button class="w-full bg-custom text-gray-100 flex flex-row justify-between px-8 py-4 text-xl">
-				<div class="flex flex-row gap-x-4 items-center">
-					<div class="w-8 h-8">
-						<svg fill="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg" aria-hidden="true">
-							<path
-								d="M5.625 3.75a2.625 2.625 0 100 5.25h12.75a2.625 2.625 0 000-5.25H5.625zM3.75 11.25a.75.75 0 000 1.5h16.5a.75.75 0 000-1.5H3.75zM3 15.75a.75.75 0 01.75-.75h16.5a.75.75 0 010 1.5H3.75a.75.75 0 01-.75-.75zM3.75 18.75a.75.75 0 000 1.5h16.5a.75.75 0 000-1.5H3.75z"
-							></path>
-						</svg>
-					</div>
-					<span class="text-lg font-medium">Ratings & Reviews</span>
-				</div>
-			</button>
-			<div class="w-full flex flex-col gap-y-4 px-4 py-3">
-				<div class="w-full grid grid-cols-2 gap-y-4 py-4">
-					<div class="col-span-1 flex flex-col gap-y-1 justify-center items-center border-r border-neutral-200">
-						<span class="text-5xl text-neutral-500 leading-tight">3.0</span>
-						<span class="text-base font-light leading-tight">Out of 5.00</span>
-						<div class="flex flex-row gap-x-0.5 items-center">
-							<button class="w-4 h-4 text-yellow-400">
-								<IconsStar />
-							</button>
-							<button class="w-4 h-4 text-yellow-400">
-								<IconsStar /></button
-							><button class="w-4 h-4 text-yellow-400">
-								<IconsStar /></button
-							><button class="w-4 h-4 text-yellow-400">
-								<IconsStar /></button
-							><button class="w-4 h-4 text-gray-300">
-								<IconsStar />
-							</button>
-						</div>
-						<span class="text-base">3 ratings</span>
-					</div>
-					<div class="col-span-1 flex flex-col gap-y-1 justify-center items-center">
-						<ul class="flex flex-col gap-y-0">
-							<li class="flex flex-row gap-x-2 items-center">
-								<div class="flex flex-row gap-x-0.5 items-center">
-									<button class="w-3 h-3 text-yellow-400">
-										<IconsStar />
-									</button>
-									<button class="w-3 h-3 text-yellow-400">
-										<IconsStar /></button
-									><button class="w-3 h-3 text-yellow-400">
-										<IconsStar /></button
-									><button class="w-3 h-3 text-yellow-400">
-										<IconsStar /></button
-									><button class="w-3 h-3 text-yellow-400">
-										<IconsStar />
-									</button>
-								</div>
-								<span class="text-neutral-500 text-sm">1</span>
-							</li>
-							<li class="flex flex-row gap-x-2 items-center">
-								<div class="flex flex-row gap-x-0.5 items-center">
-									<button class="w-3 h-3 text-yellow-400">
-										<IconsStar />
-									</button>
-									<button class="w-3 h-3 text-yellow-400">
-										<IconsStar /></button
-									><button class="w-3 h-3 text-yellow-400">
-										<IconsStar /></button
-									><button class="w-3 h-3 text-yellow-400">
-										<IconsStar /></button
-									><button class="w-3 h-3 text-gray-300">
-										<IconsStar />
-									</button>
-								</div>
-								<span class="text-neutral-500 text-sm">0</span>
-							</li>
-							<li class="flex flex-row gap-x-2 items-center">
-								<div class="flex flex-row gap-x-0.5 items-center">
-									<button class="w-3 h-3 text-yellow-400">
-										<IconsStar />
-									</button>
-									<button class="w-3 h-3 text-yellow-400">
-										<IconsStar /></button
-									><button class="w-3 h-3 text-yellow-400">
-										<IconsStar /></button
-									><button class="w-3 h-3 text-gray-300">
-										<IconsStar /></button
-									><button class="w-3 h-3 text-gray-300">
-										<IconsStar />
-									</button>
-								</div>
-								<span class="text-neutral-500 text-sm">1</span>
-							</li>
-							<li class="flex flex-row gap-x-2 items-center">
-								<div class="flex flex-row gap-x-0.5 items-center">
-									<button class="w-3 h-3 text-yellow-400">
-										<IconsStar />
-									</button>
-									<button class="w-3 h-3 text-yellow-400">
-										<IconsStar /></button
-									><button class="w-3 h-3 text-gray-300">
-										<IconsStar /></button
-									><button class="w-3 h-3 text-gray-300">
-										<IconsStar /></button
-									><button class="w-3 h-3 text-gray-300">
-										<IconsStar />
-									</button>
-								</div>
-								<span class="text-neutral-500 text-sm">0</span>
-							</li>
-							<li class="flex flex-row gap-x-2 items-center">
-								<div class="flex flex-row gap-x-0.5 items-center">
-									<button class="w-3 h-3 text-yellow-400">
-										<IconsStar />
-									</button>
-									<button class="w-3 h-3 text-gray-300">
-										<IconsStar /></button
-									><button class="w-3 h-3 text-gray-300">
-										<IconsStar /></button
-									><button class="w-3 h-3 text-gray-300">
-										<IconsStar /></button
-									><button class="w-3 h-3 text-gray-300">
-										<IconsStar />
-									</button>
-								</div>
-								<span class="text-neutral-500 text-sm">1</span>
-							</li>
-						</ul>
-					</div>
-				</div>
-			</div>
+		<div class="w-full flex flex-col border border-t-0 pt-6 sm:pt-8 pb-4 px-2 sm:px-4 dark:text-neutral-200 dark:border-neutral-600">
+			<p class="text-sm sm:text-base">
+				Lorem ipsum dolor sit amet consectetur adipisicing elit. Ut ipsa dicta inventore consequatur excepturi dolore veniam quidem
+				quaerat ipsum quo, mollitia nobis, rerum officia! Et deserunt tempora sapiente? Natus, perspiciatis?
+			</p>
+			<p class="text-sm sm:text-base">
+				Lorem ipsum dolor sit amet consectetur adipisicing elit. Error totam a tempore blanditiis unde architecto recusandae
+				delectus corrupti facere dolorem distinctio, labore nisi rerum. Officia iure facilis perspiciatis repudiandae reprehenderit.
+			</p>
 		</div>
 	</div>
 </template>
--- client/pages/index/products/product-[productId].vue
+++ client/pages/index/products/product-[productId].vue
@@ -2,7 +2,7 @@
 	<section class="w-full flex flex-col py-4 px-4 md:px-8 lg:px-20 2xl:px-44">
 		<div class="w-full grid grid-cols-1 xl:grid-cols-12 gap-x-6 gap-y-12">
 			<div class="xl:col-span-9 flex flex-col gap-y-4">
-				<div class="w-full grid grid-cols-1 md:grid-cols-10 gap-x-4 gap-y-8 border-b border-neutral-200 pb-8">
+				<div class="w-full grid grid-cols-1 md:grid-cols-10 gap-x-4 gap-y-8 pb-8">
 					<div class="md:col-span-4 flex flex-col gap-y-4">
 						<ul class="w-full flex overflow-x-auto">
 							<li class="w-full flex flex-col border border-neutral-200 rounded">
@@ -12,163 +12,163 @@
 						<ul class="w-full flex flex-row gap-x-2 overflow-x-auto items-center">
 							<li class="">
 								<button
-									class="w-24 h-24 p-1 border border-neutral-200 focus:border-custom rounded flex flex-col items-center justify-center"
+									class="w-24 h-24 p-1 border border-neutral-200 focus:border-custom rounded flex flex-col items-center justify-center dark:border-neutral-700 dark:focus:border-custom"
 								>
-									<NuxtImg src="/items/phone.png" class="h-full" />
+									<NuxtImg src="/items/phone.png" class="h-full rounded-sm" />
 								</button>
 							</li>
 							<li class="">
 								<button
-									class="w-24 h-24 p-1 border border-neutral-200 focus:border-custom rounded flex flex-col items-center justify-center"
+									class="w-24 h-24 p-1 border border-neutral-200 focus:border-custom rounded flex flex-col items-center justify-center dark:border-neutral-700 dark:focus:border-custom"
 								>
-									<NuxtImg src="/items/keyboard.png" class="h-full" />
+									<NuxtImg src="/items/keyboard.png" class="h-full rounded-sm" />
 								</button>
 							</li>
 							<li class="">
 								<button
-									class="w-24 h-24 p-1 border border-neutral-200 focus:border-custom rounded flex flex-col items-center justify-center"
+									class="w-24 h-24 p-1 border border-neutral-200 focus:border-custom rounded flex flex-col items-center justify-center dark:border-neutral-700 dark:focus:border-custom"
 								>
-									<NuxtImg src="/items/tecno.jpg" class="h-full" />
+									<NuxtImg src="/items/tecno.jpg" class="h-full rounded-sm" />
 								</button>
 							</li>
 						</ul>
 					</div>
 					<div class="md:col-span-6 flex flex-col gap-y-4">
-						<div class="title w-full flex flex-col gap-y-2 pb-2 border-b border-neutral-200">
-							<h2 class="text-2xl font-bold line-clamp-2 text-neutral-700">ASUS ROG Phone 5 Dual Sim S888 Phantom</h2>
+						<div
+							class="title w-full flex flex-col gap-y-2 pb-2 px-2 sm:px-0 border-b border-neutral-200 dark:border-neutral-600"
+						>
+							<h2 class="text-xl sm:text-2xl font-normal leading-none line-clamp-2 text-neutral-700 dark:text-neutral-200">
+								ASUS ROG Phone 5 Dual Sim S888 Phantom
+							</h2>
 							<div class="w-full flex flex-row items-center justify-between py-1">
 								<div class="">
 									<ContentsRatings />
 								</div>
-								<div class="flex flex-row gap-x-1 items-center pr-2">
-									<button class="flex flex-row items-center gap-x-1 text-custom">
-										<div class="w-3 h-3">
-											<IconsHeart />
-										</div>
-										<small class="text-sm font-semibold">Add to Wishlist</small>
-									</button>
-								</div>
 							</div>
 						</div>
-						<div class="brand w-full flex flex-row gap-x-4 items-center">
-							<div class="w-20 h-20 flex items-center justify-center border border-neutral-200 rounded">
+						<div class="brand w-full flex flex-row gap-x-4 items-center px-2 sm:px-0">
+							<div class="w-16 h-16 flex items-center justify-center border border-neutral-200 rounded">
 								<NuxtImg src="/items/phone.png" class="h-full w-auto" />
 							</div>
-							<div class="flex flex-col">
-								<small class="text-sm text-neutral-500">Brand</small>
-								<span class="text-base font-semibold uppercase">Hermes</span>
-								<button class="text-sm text-neutral-500">View other products</button>
+							<div class="flex flex-col text-neutral-600 dark:text-neutral-200">
+								<small class="text-sm leading-none">Brand</small>
+								<span class="text-base font-semibold uppercase text-neutral-800 dark:text-neutral-200">Hermes</span>
+								<button class="text-sm leading-none">View other products</button>
 							</div>
 						</div>
-						<div class="w-full flex flex-col gap-y-2.5 mt-3 border-b border-neutral-200 pb-2.5">
-							<div class="w-full grid grid-cols-10 gap-x-4 items-center">
-								<div class="col-span-2">
-									<small class="text-base text-neutral-500">Price</small>
-								</div>
-								<div class="col-span-8 flex flex-row items-center gap-x-4">
-									<span class="text-xl text-neutral-500 font-medium line-through">$351.00</span>
+						<div class="w-full flex flex-col gap-y-2.5 px-2 sm:px-0 border-b border-neutral-200 py-3.5 dark:border-neutral-600">
+							<div class="flex flex-row items-center gap-x-4">
+								<span class="text-2xl font-semibold dark:text-neutral-100">$351.00</span>
+								<div class="flex flex-row items-center gap-x-4 mt-1">
+									<span class="text-base text-neutral-500 font-medium line-through dark:text-neutral-200">$351.00</span>
 									<div class="flex flex-row gap-x-2 items-center">
-										<button class="px-2 bg-[tomato] text-white uppercase text-sm shadow-md">off 5%</button>
+										<button class="px-2 py-0.5 rounded-sm bg-[tomato] text-white uppercase text-xs">off 5%</button>
 									</div>
 								</div>
 							</div>
-							<div class="w-full grid grid-cols-10 gap-x-4 items-center">
-								<div class="col-span-2">
-									<small class="text-base text-neutral-500">Discounted Price</small>
-								</div>
-								<div class="col-span-8 flex flex-row items-center gap-x-4">
-									<span class="text-2xl text-custom font-semibold">$351.00</span>
-								</div>
+						</div>
+						<div class="w-full flex flex-col gap-y-2 py-1 px-2 sm:px-0">
+							<div class="w-full flex flex-row items-center gap-x-2">
+								<Icon name="mdi:check-circle" class="text-lg text-emerald-600" />
+								<span class="text-sm text-neutral-600 dark:text-neutral-100">In stock</span>
+							</div>
+							<div class="w-full flex flex-row items-center gap-x-2">
+								<Icon name="mdi:plus" class="text-base text-neutral-600 dark:text-neutral-100" />
+								<span class="text-sm text-neutral-600 dark:text-neutral-200"
+									>Shipping to Anywhere from <span class="text-sm text-neutral-800 dark:text-white">Nakuru</span>
+								</span>
 							</div>
 						</div>
-						<div class="variant w-full flex flex-col gap-y-2 mt-1">
-							<div class="w-full grid grid-cols-10 gap-x-4 items-center">
+						<div class="variant w-full flex flex-col gap-y-4 py-1 px-2 sm:px-0">
+							<h4 class="text-base uppercase dark:text-neutral-100">Variants Available</h4>
+							<div class="w-full grid grid-cols-12 gap-x-4 items-center">
 								<div class="col-span-2">
-									<small class="text-base text-neutral-500">Memory</small>
+									<small class="text-sm text-neutral-500 dark:text-neutral-100">Memory</small>
 								</div>
-								<div class="col-span-8 flex flex-row items-center gap-x-4">
-									<button class="px-3.5 py-1 border border-neutral-200 rounded text-base text-neutral-700 font-semibold">
+								<div class="col-span-10 flex flex-row items-center gap-x-4 flex-wrap">
+									<button
+										class="px-3.5 py-0.5 border border-neutral-200 rounded text-sm text-neutral-700 focus:border-custom font-semibold dark:border-neutral-700 dark:text-neutral-100"
+									>
+										16 GB
+									</button>
+									<button
+										class="px-3.5 py-0.5 border border-neutral-200 rounded text-sm text-neutral-700 focus:border-custom font-semibold dark:border-neutral-700 dark:text-neutral-100"
+									>
+										32 GB
+									</button>
+									<button
+										class="px-3.5 py-0.5 border border-neutral-200 rounded text-sm text-neutral-700 focus:border-custom font-semibold dark:border-neutral-700 dark:text-neutral-100"
+									>
 										256 GB
 									</button>
 								</div>
 							</div>
-						</div>
-						<div class="grid md:grid-cols-2 gap-x-4 gap-y-4 mt-2">
-							<div class="px-4 py-3 border-custom border rounded bg-[red]/5 flex flex-col gap-y-2">
-								<div class="flex flex-row gap-x-2 items-center">
-									<div class="w-5 h-5 text-green-500">
-										<svg fill="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg" aria-hidden="true">
-											<path
-												clip-rule="evenodd"
-												fill-rule="evenodd"
-												d="M2.25 12c0-5.385 4.365-9.75 9.75-9.75s9.75 4.365 9.75 9.75-4.365 9.75-9.75 9.75S2.25 17.385 2.25 12zm13.36-1.814a.75.75 0 10-1.22-.872l-3.236 4.53L9.53 12.22a.75.75 0 00-1.06 1.06l2.25 2.25a.75.75 0 001.14-.094l3.75-5.25z"
-											></path>
-										</svg>
-									</div>
-									<small class="text-sm">In Stock</small>
+							<div class="w-full grid grid-cols-12 gap-x-4 items-center">
+								<div class="col-span-2">
+									<small class="text-sm text-neutral-500 dark:text-neutral-100">Color</small>
 								</div>
-								<div class="flex flex-row gap-x-2 items-center">
-									<div class="w-5 h-5 text-custom">
-										<svg fill="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg" aria-hidden="true">
-											<path
-												clip-rule="evenodd"
-												fill-rule="evenodd"
-												d="M4.804 21.644A6.707 6.707 0 006 21.75a6.721 6.721 0 003.583-1.029c.774.182 1.584.279 2.417.279 5.322 0 9.75-3.97 9.75-9 0-5.03-4.428-9-9.75-9s-9.75 3.97-9.75 9c0 2.409 1.025 4.587 2.674 6.192.232.226.277.428.254.543a3.73 3.73 0 01-.814 1.686.75.75 0 00.44 1.223zM8.25 10.875a1.125 1.125 0 100 2.25 1.125 1.125 0 000-2.25zM10.875 12a1.125 1.125 0 112.25 0 1.125 1.125 0 01-2.25 0zm4.875-1.125a1.125 1.125 0 100 2.25 1.125 1.125 0 000-2.25z"
-											></path>
-										</svg>
-									</div>
-									<small class="text-sm">Contact Admin</small>
-								</div>
-							</div>
-							<div class="px-4 py-3 rounded bg-gray-200 flex flex-col gap-y-1.5">
-								<div class="flex flex-row gap-x-2 items-center">
-									<div class="w-7 h-7 text-neutral-500">
-										<svg fill="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg" aria-hidden="true">
-											<path
-												d="M3.375 4.5C2.339 4.5 1.5 5.34 1.5 6.375V13.5h12V6.375c0-1.036-.84-1.875-1.875-1.875h-8.25zM13.5 15h-12v2.625c0 1.035.84 1.875 1.875 1.875h.375a3 3 0 116 0h3a.75.75 0 00.75-.75V15z"
-											></path>
-											<path
-												d="M8.25 19.5a1.5 1.5 0 10-3 0 1.5 1.5 0 003 0zM15.75 6.75a.75.75 0 00-.75.75v11.25c0 .087.015.17.042.248a3 3 0 015.958.464c.853-.175 1.522-.935 1.464-1.883a18.659 18.659 0 00-3.732-10.104 1.837 1.837 0 00-1.47-.725H15.75z"
-											></path>
-											<path d="M19.5 19.5a1.5 1.5 0 10-3 0 1.5 1.5 0 003 0z"></path>
-										</svg>
-									</div>
-									<small class="text-base font-semibold leading-tight">Estimated Delivery time</small>
-								</div>
-								<div class="flex flex-row gap-x-2 px-8">
-									<span class="text-base text-neutral-600 leading-tight">1 - 5 days</span>
+								<div class="col-span-10 flex flex-row items-center gap-x-4 flex-wrap">
+									<button
+										class="px-3.5 py-0.5 border border-neutral-200 rounded text-sm text-neutral-700 focus:border-custom font-semibold dark:border-neutral-700 dark:text-neutral-100"
+									>
+										Black
+									</button>
+									<button
+										class="px-3.5 py-0.5 border border-neutral-200 rounded text-sm text-neutral-700 focus:border-custom font-semibold dark:border-neutral-700 dark:text-neutral-100"
+									>
+										Blue
+									</button>
+									<button
+										class="px-3.5 py-0.5 border border-neutral-200 rounded text-sm text-neutral-700 focus:border-custom font-semibold dark:border-neutral-700 dark:text-neutral-100"
+									>
+										Pink
+									</button>
 								</div>
 							</div>
 						</div>
-						<div class="quantity w-full grid grid-cols-10 gap-x-4 items-center mt-2">
-							<div class="col-span-2">
-								<small class="text-base text-neutral-500">Quantity</small>
+						<div class="quantity w-full flex flex-col gap-y-1 py-3 border-t border-neutral-200 dark:border-neutral-600">
+							<div class="">
+								<small class="text-sm dark:text-neutral-100">Quantity:</small>
 							</div>
-							<div class="col-span-8 flex flex-row gap-x-0 -space-x-px">
-								<button class="px-2 py-1 text-neutral-600 border border-neutral-200 rounded-l">
-									<div class="w-5 h-5">
-										<IconsMinus />
-									</div>
-								</button>
-								<input
-									type="number"
-									name="quantity"
-									id="quantity"
-									class="border border-neutral-200 py-1 text-lg text-center w-[6rem] outline-none focus:ring-1 focus:ring-custom focus:border-custom transition duration-200"
-									value="1"
-								/>
-								<button class="px-2 py-1 text-neutral-600 border border-neutral-200 rounded-r">
-									<div class="w-5 h-5">
-										<IconsPlus />
-									</div>
-								</button>
+							<div class="col-span-8 flex flex-row gap-x-0">
+								<div class="relative w-[9rem] z-0">
+									<button
+										class="z-10 absolute left-0 top-0 bottom-0 px-2 py-1 text-neutral-600 border-r border-neutral-200 rounded-l hover:text-custom focus:text-custom transition duration-200 dark:border-neutral-600 dark:hover:text-custom dark:text-neutral-100"
+									>
+										<Icon name="mdi:minus" class="text-base" />
+									</button>
+									<input
+										type="number"
+										name="quantity"
+										id="quantity"
+										class="border border-neutral-200 rounded py-1 text-base text-center w-full outline-none hover:border-custom focus:border-custom transition duration-200 dark:bg-inherit dark:border-neutral-600 dark:text-neutral-100 dark:hover:border-custom dark:focus:border-custom"
+										value="1"
+										min="1"
+									/>
+									<button
+										class="z-10 absolute right-0 top-0 bottom-0 px-2 py-1 text-neutral-600 border-l border-neutral-200 hover:text-custom focus:text-custom transition duration-200 dark:border-neutral-600 dark:hover:text-custom dark:text-neutral-100"
+									>
+										<Icon name="mdi:plus" class="text-base" />
+									</button>
+								</div>
 							</div>
 						</div>
-						<div class="w-full flex flex-col gap-y-2 mt-2">
+						<div class="w-full flex flex-row gap-x-4 mt-1">
 							<button
-								class="text-lg text-white bg-c-dark/80 rounded py-2.5 font-medium hover:bg-c-dark/95 transition duration-200"
+								class="text-sm sm:text-base text-custom border border-custom rounded px-6 py-2.5 font-medium transition duration-200"
 							>
 								Add to Cart
+							</button>
+							<button
+								class="text-sm sm:text-base text-white bg-custom rounded px-6 py-2.5 font-medium hover:bg-custom/90 transition duration-200"
+							>
+								Buy now
+							</button>
+							<!-- Add to wishlist -->
+							<button
+								class="px-6 py-2.5 flex items-center justify-center border border-neutral-300 rounded text-neutral-600 hover:text-custom hover:border-custom focus:text-custom focus:border-custom transition-colors duration-200 dark:border-neutral-500 dark:hover:border-custom dark:focus:border-custom dark:text-neutral-100 dark:hover:text-custom dark:focus:text-custom"
+							>
+								<Icon name="mdi:heart-outline" class="text-lg sm:text-xl" />
 							</button>
 						</div>
 						<!--  -->
@@ -178,7 +178,7 @@
 					<ContainersProductDetails />
 				</div>
 				<div class="mt-10 flex flex-col gap-y-4 w-full">
-					<h2 class="text-xl sm:text-2xl md:text-3xl font-semibold text-neutral-600">More Items to Explore</h2>
+					<h2 class="text-lg font-medium uppercase dark:text-neutral-100">More Items to Explore</h2>
 					<div class="w-full grid grid-cols-2 sm:grid-cols-3 md:grid-cols-4 gap-4">
 						<ListItemCard />
 						<ListItemCard />
@@ -197,5 +197,11 @@
 <style scoped>
 ::-webkit-scrollbar:horizontal {
 	height: 0px;
+}
+
+input[type="number"]::-webkit-inner-spin-button,
+input[type="number"]::-webkit-outer-spin-button {
+	-webkit-appearance: none;
+	margin: 0;
 }
 </style>
