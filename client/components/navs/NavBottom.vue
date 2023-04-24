68acd0820615578cb76031ccac4c3d202a5b87c2
Amos Kipyegon <amoskip@kabarak.ac.ke>
2023-04-24 12:09:34 +0300

    chore: update mobile menu navbar

--- client/components/navs/NavBottom.vue
+++ client/components/navs/NavBottom.vue
@@ -27,14 +27,14 @@ const closeMenu = (): void => {
 			id="mobi-nav"
 			class="z-10 flex flex-row justify-between items-center py-2 px-4 sm:px-6 border-t border-neutral-300 dark:border-neutral-700 bg-white dark:bg-c-dark"
 		>
-			<button>
+			<NuxtLink to="/">
 				<Icon name="mdi:home-outline" class="text-2xl" />
 				<small class="text-xs sm:text-sm font-medium sm:font-semibold">Home</small>
-			</button>
-			<button>
+			</NuxtLink>
+			<NuxtLink to="/products">
 				<Icon name="mdi:view-grid-outline" class="text-2xl" />
 				<small class="text-xs sm:text-sm font-medium sm:font-semibold">Products</small>
-			</button>
+			</NuxtLink>
 			<button>
 				<Icon name="mdi:cart-outline" class="text-2xl" />
 				<small class="text-xs sm:text-sm font-medium sm:font-semibold">Cart</small>
@@ -62,42 +62,42 @@ const closeMenu = (): void => {
 				<div
 					class="h-[calc(100vh-225px)] absolute bottom-0 w-full bg-c-base z-10 backdrop-filter backdrop-blur-md rounded-t-md py-6 px-1 flex flex-col dark:bg-c-dark dark:backdrop-filter dark:backdrop-blur-md overflow-y-auto"
 				>
-					<nav class="w-full flex flex-col gap-y-2 px-1">
+					<nav class="menu w-full flex flex-col gap-y-2 px-1">
 						<NuxtLink to="/" class="hover:bg-white px-2 rounded dark:hover:bg-alt-dark transition-colors duration-200">
 							<div class="flex flex-row items-center gap-4 py-2.5">
-								<Icon name="mdi:home-outline" class="text-xl sm:text-2xl text-neutral-600 dark:text-c-mode" />
+								<Icon name="mdi:home-outline" class="text-2xl text-neutral-600 dark:text-c-mode" />
 								<span class="block text-base">Home</span>
 							</div>
 						</NuxtLink>
 						<NuxtLink to="/products" class="hover:bg-white px-2 rounded dark:hover:bg-alt-dark transition-colors duration-200">
 							<div class="flex flex-row items-center gap-4 py-2.5">
-								<Icon name="mdi:view-grid-outline" class="text-xl sm:text-2xl text-neutral-600 dark:text-c-mode" />
+								<Icon name="mdi:view-grid-outline" class="text-2xl text-neutral-600 dark:text-c-mode" />
 								<span class="block text-base"> All Products</span>
 							</div>
 						</NuxtLink>
 						<NuxtLink to="/shops" class="hover:bg-white px-2 rounded dark:hover:bg-alt-dark transition-colors duration-200">
 							<div class="flex flex-row items-center gap-4 py-2.5">
-								<Icon name="mdi:shopping-search-outline" class="text-xl sm:text-2xl text-neutral-600 dark:text-c-mode" />
+								<Icon name="mdi:shopping-search-outline" class="text-2xl text-neutral-600 dark:text-c-mode" />
 								<span class="block text-base">Shops</span>
 							</div>
 						</NuxtLink>
 						<NuxtLink to="/shops" class="hover:bg-white px-2 rounded dark:hover:bg-alt-dark transition-colors duration-200">
 							<div class="flex flex-row items-center gap-4 py-2.5">
-								<Icon name="mdi:cart-percent" class="text-xl sm:text-2xl text-neutral-600 dark:text-c-mode" />
+								<Icon name="mdi:cart-percent" class="text-2xl text-neutral-600 dark:text-c-mode" />
 								<span class="block text-base">Offers</span>
 							</div>
 						</NuxtLink>
 
 						<NuxtLink to="/shops" class="hover:bg-white px-2 rounded dark:hover:bg-alt-dark transition-colors duration-200">
 							<div class="flex flex-row items-center gap-4 py-2.5">
-								<Icon name="mdi:arrow-top-right" class="text-xl sm:text-2xl text-neutral-600 dark:text-c-mode" />
+								<Icon name="mdi:arrow-top-right" class="text-2xl text-neutral-600 dark:text-c-mode" />
 								<span class="block text-base">Men Clothing & Fashion</span>
 							</div>
 						</NuxtLink>
 
 						<NuxtLink to="/shops" class="hover:bg-white px-2 rounded dark:hover:bg-alt-dark transition-colors duration-200">
 							<div class="flex flex-row items-center gap-4 py-2.5">
-								<Icon name="mdi:arrow-top-right" class="text-xl sm:text-2xl text-neutral-600 dark:text-c-mode" />
+								<Icon name="mdi:arrow-top-right" class="text-2xl text-neutral-600 dark:text-c-mode" />
 								<span class="block text-base">Computer & Accessories</span>
 							</div>
 						</NuxtLink>
@@ -108,7 +108,7 @@ const closeMenu = (): void => {
 							class="w-full flex flex-row py-0.5 px-3.5 hover:bg-white rounded dark:hover:bg-alt-dark transition-colors duration-200"
 						>
 							<div class="flex flex-row items-center gap-4 py-2">
-								<Icon name="mdi:account-outline" class="text-xl sm:text-2xl text-neutral-600 dark:text-c-mode" />
+								<Icon name="mdi:account-outline" class="text-2xl text-neutral-600 dark:text-c-mode" />
 								<span class="block text-base">Account</span>
 							</div>
 						</NuxtLink>
@@ -118,14 +118,11 @@ const closeMenu = (): void => {
 						>
 							<Transition mode="out-in">
 								<div v-if="colorBg" class="flex flex-row w-full gap-x-3 items-center">
-									<Icon name="mdi:white-balance-sunny" class="text-xl sm:text-2xl text-neutral-600 dark:text-c-mode" />
+									<Icon name="mdi:white-balance-sunny" class="text-2xl text-neutral-600 dark:text-c-mode" />
 									<span class="text-base">Toggle Dark Mode</span>
 								</div>
 								<div v-else class="flex flex-row w-full gap-x-3 items-center">
-									<Icon
-										name="mdi:moon-waning-crescent"
-										class="text-xl sm:text-2xl -rotate-45 text-neutral-600 dark:text-c-mode"
-									/>
+									<Icon name="mdi:moon-waning-crescent" class="text-2xl -rotate-45 text-neutral-600 dark:text-c-mode" />
 									<span class="text-base">Toggle Light Mode</span>
 								</div>
 							</Transition>
@@ -138,12 +135,23 @@ const closeMenu = (): void => {
 </template>
 
 <style scoped>
-#mobi-nav button {
-	@apply relative flex flex-col gap-y-1 items-center justify-center px-2 py-1 text-neutral-700 hover:text-custom dark:text-c-mode dark:hover:text-custom dark:first:text-custom;
+#mobi-nav button,
+#mobi-nav a {
+	@apply relative flex flex-col gap-y-1 items-center justify-center px-2 py-1 text-neutral-700 hover:text-custom dark:text-c-mode dark:hover:text-custom;
 }
 button:focus small,
 button:focus .icon {
 	@apply text-custom dark:text-custom;
+}
+
+a.router-link-exact-active,
+a.router-link-exact-active small,
+a.router-link-exact-active .icon,
+a.router-link-exact-active span {
+	@apply text-custom dark:text-custom;
+}
+nav.menu a.router-link-exact-active {
+	@apply bg-white dark:bg-alt-dark;
 }
 .show-menu-transition-enter-from,
 .show-menu-transition-leave-to {
